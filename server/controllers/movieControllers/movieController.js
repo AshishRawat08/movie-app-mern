2d0228a151966abce8bc5b730a483ee6edfb23d6
AshishRawat08 <rawashu18@gmail.com>
2024-02-28 17:04:04 +0530

    apis--- getallmovies, update/editmovie, deletemovie, getsinglemovie

--- server/controllers/movieControllers/movieController.js
+++ server/controllers/movieControllers/movieController.js
@@ -30,3 +30,96 @@ exports.createMovie = async (req, res) => {
     }
   }
 };
+
+// get all movies data
+exports.getAllUserMovies = async (req, res) => {
+  const page = req.query.page || 1;
+  const ITEM_PER_PAGE = 2;
+  const search = req.query.search || "";
+  const sort = req.query.sort || "";
+
+  const query = {
+    movieName: { $regex: search, $options: "i" },
+  };
+
+  try {
+    const skip = (page - 1) * ITEM_PER_PAGE;
+
+    const allUserMovieCount = await movieDB.countDocuments(query);
+
+    const allUserMovieData = await movieDB
+      .find(query)
+      .limit(ITEM_PER_PAGE)
+      .skip(skip)
+      .sort({ _id: sort == "new" ? -1 : 1 });
+
+    const pageCount = Math.ceil(allUserMovieCount / ITEM_PER_PAGE);
+
+    res.status(200).json({
+      Pagination: { allUserMovieCount, pageCount },
+      allUserMovieData,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ error: "Internal error" });
+  }
+};
+
+// upate or edit movie
+exports.updateMovie = async (req, res) => {
+  const { id } = req.params;
+  const file = req?.file ? req?.file?.path : "";
+  const { movieName, publishYear, image } = req.body;
+
+  var upload;
+  try {
+    if (file) {
+      upload = await cloudinary?.uploader?.upload(file);
+    }
+
+    let dynamicImg = file ? upload?.secure_url : image;
+    const moviesUpdate = await movieDB.findByIdAndUpdate(
+      { _id: id },
+      {
+        userID: req?.userMainId,
+        movieName,
+        image: dynamicImg,
+        publishYear,
+      },
+      { new: true }
+    );
+    await moviesUpdate.save();
+    res.status(200).json({ msg: "Movie successfully updated", moviesUpdate });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ error: "Internal error" });
+  }
+};
+
+// delete movie
+exports.deleteMovie = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const deleteMovieData = await movieDB.findByIdAndDelete({ _id: id });
+    res
+      .status(200)
+      .json({ msg: "movie successfully deleted", deleteMovieData });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ error: "Internal error" });
+  }
+};
+
+// get single movie data
+exports.getSingleMovie = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const getSingleMovieData = await movieDB.findOne({ _id: id });
+    res.status(200).json(getSingleMovieData);
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ error: "Internal error" });
+  }
+};
--- server/routes/movie/movieRoutes.js
+++ server/routes/movie/movieRoutes.js
@@ -11,4 +11,20 @@ router.post(
   moviesController.createMovie
 );
 
+// get all movie data
+router.get("/getallmovies", moviesController.getAllUserMovies);
+
+//edit or update movie data
+router.patch(
+  "/update/:id",
+  [userAuthenticate, movieUpload.single("image")],
+  moviesController.updateMovie
+);
+
+//delete movie data
+router.delete("/delete/:id", userAuthenticate, moviesController.deleteMovie);
+
+// movie detail
+router.get("/detail/:id", moviesController.getSingleMovie);
+
 module.exports = router;
